ba22e71baecb408fd1cda663c1b185405c9978c7
rahulp <rahulp5875@gmail.com>
2023-05-16 08:46:09 +0530

    Add notes list and design row for each note.

--- app/src/main/java/com/example/composenoteapp/screens/NoteScreen.kt
+++ app/src/main/java/com/example/composenoteapp/screens/NoteScreen.kt
@@ -1,20 +1,23 @@
 package com.example.composenoteapp.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.composenoteapp.R
-import androidx.compose.ui.res.stringResource
 import com.example.composenoteapp.components.NoteButton
 import com.example.composenoteapp.components.NoteTextInputField
 import com.example.composenoteapp.models.Note
@@ -76,6 +79,44 @@ fun NoteScreen(
                     }
                 }
             )
+        }
+
+        // Notes list
+
+        Divider(modifier = Modifier.padding(8.dp))
+
+        LazyColumn(){
+            items(notes){ note ->
+                NoteRow(note = note, onNoteClicked = {})
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onNoteClicked: (Note) -> Unit
+){
+    Surface(
+        modifier = modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .clip(
+                RoundedCornerShape(topEnd = 32.dp, bottomStart = 32.dp)
+            ),
+        color = Color(0XFFDFE6EB),
+        elevation = 8.dp
+    ) {
+        Column(modifier = modifier
+            .clickable { onNoteClicked(note) }
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        horizontalAlignment = Alignment.Start,
+        ) {
+            Text(text = note.title, style = MaterialTheme.typography.subtitle2)
+            Text(text = note.desc, style = MaterialTheme.typography.subtitle1)
+            Text(text = note.entryTimeStamp.toString(), style = MaterialTheme.typography.caption)
         }
     }
 }
