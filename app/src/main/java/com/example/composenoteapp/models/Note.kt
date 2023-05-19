654eecf55e6baab4b6a72b81fb2c2a40a870b436
rahulp <rahulp5875@gmail.com>
2023-05-19 07:41:43 +0530

    Create note entity.

--- app/src/main/java/com/example/composenoteapp/models/Note.kt
+++ app/src/main/java/com/example/composenoteapp/models/Note.kt
@@ -1,10 +1,18 @@
 package com.example.composenoteapp.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity(tableName = "note_tbl")
 data class Note(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+    @ColumnInfo(name = "note_title")
     val title: String,
+    @ColumnInfo(name = "note_desc")
     val desc: String,
+    @ColumnInfo(name = "note_timestamp")
     val entryTimeStamp: Long = System.currentTimeMillis()
 )
